ccb705764677a3f6785b53498fbdf176b18374aa
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-03 11:22:20 +0530

    mern

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,9 @@ const UserSchema = new mongoose.Schema({
   phoneNumber: { type: String, required: true, unique: true },
   verificationCode: { type: String },
   isVerified: { type: Boolean, default: false },
+  username: { type: String, required: true, unique: true },
+  profilePicture: { type: String }, // URL to profile picture
+  status: { type: String, default: 'Available' }, // User status
 });
 
 module.exports = mongoose.model('User', UserSchema);
