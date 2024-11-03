aeb7d9d10a4898bdcb69e53c41d58b1bc6765f0e
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-03 10:43:10 +0530

    mermn

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Socket.io setup
 io.on('connection', (socket) => {
@@ -26,7 +29,12 @@ io.on('connection', (socket) => {
   });
 });
 
-// Define routes here...
+// Sample API route
+app.get('/api/messages', (req, res) => {
+  // Logic to get messages from the database
+  res.json({ messages: [] });
+});
 
+// Start server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
